2c168d69def17bd64c57089f47a7028e1644b328
Aaayush-7 <aaayushkhanal999@gmail.com>
2026-08-25 21:39:44 +0545

    Implemented user registration API

---
+++ users/serializers.py
@@ -0,0 +1,20 @@
+from django.contrib.auth import get_user_model
+from django.contrib.auth.password_validation import validate_password
+from rest_framework import serializers
+
+
+User = get_user_model()
+
+
+class UserRegistrationSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(
+        write_only=True,
+        validators=[validate_password]
+    )
+
+    class Meta:
+        model = User
+        fields = ("email", "password", "first_name", "last_name")
+
+    def create(self, validated_data):
+        return User.objects.create_user(**validated_data)
---
+++ users/views.py
@@ -0,0 +1,8 @@
+from rest_framework import generics
+
+from .serializers import UserRegistrationSerializer
+
+
+class UserRegistrationView(generics.CreateAPIView):
+    serializer_class = UserRegistrationSerializer
+# Create your views here.
