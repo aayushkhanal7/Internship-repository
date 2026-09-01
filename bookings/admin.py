6fab5f880234677fe4ea6b1483c519a9fdb926a4
Aaayush-7 <aaayushkhanal999@gmail.com>
2026-09-01 21:28:40 +0545

    Configured Django admin for vehicles and bookings

--- bookings/admin.py
+++ bookings/admin.py
@@ -1,3 +1,34 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Booking
+
+
+@admin.register(Booking)
+class BookingAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "user",
+        "vehicle",
+        "start_date",
+        "end_date",
+        "total_price",
+        "status",
+        "created_at",
+    )
+
+    list_filter = (
+        "status",
+        "start_date",
+        "end_date",
+    )
+
+    search_fields = (
+        "user__email",
+        "vehicle__name",
+    )
+
+    readonly_fields = (
+        "total_price",
+        "created_at",
+        "updated_at",
+    )
--- vehicles/admin.py
+++ vehicles/admin.py
@@ -3,4 +3,20 @@ from django.contrib import admin
 from .models import Vehicle
 
 
-admin.site.register(Vehicle)
+@admin.register(Vehicle)
+class VehicleAdmin(admin.ModelAdmin):
+    list_display = (
+        "name",
+        "vehicle_type",
+        "price_per_day",
+        "owner",
+    )
+
+    list_filter = (
+        "vehicle_type",
+    )
+
+    search_fields = (
+        "name",
+        "owner__email",
+    )
